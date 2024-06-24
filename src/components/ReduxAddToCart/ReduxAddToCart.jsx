16558978174c2b5ad5fe297b4d10100be089c168
Harsh-svg988 <harshprabhakar305.com>
2024-06-24 11:32:52 +0530

    deepCopyThroughSpreadOperator

--- src/components/ReduxAddToCart/ReduxAddToCart.jsx
+++ src/components/ReduxAddToCart/ReduxAddToCart.jsx
@@ -1,11 +1,26 @@
+import { useDispatch } from "react-redux";
+
 function AddToCart({ product }) {
     console.log("add to cart", product.id)
+    // use selector
+    let dispatch = useDispatch();
     function increase() {
         //dispatch({type: , payload: })
+        dispatch({ type: "ADD_TO_CART", payload: product.id });
     }
+
     function decrease() {
      //dispatch({type: , payload: })
+        dispatch({ type: "REMOVE_FROM_CART", payload: product.id });
     }
+    let cart = useSelector((state) =>{
+        return state.cart;
+    })
+
+
+
+
+
     const quantity = cart[product.id] ? cart[product.id].quantity : 0;
     if (quantity === 0) {
         return (
--- src/store.js
+++ src/store.js
@@ -1,11 +1,65 @@
 import {createStore} from  "redux";
+import {act} from "react";
+import {omit} from "lodash";
 function cartReducer(state = {items:{}},action){
     switch(action.type){
         case "ADD_TO_CART":{
-            
-
+            const product = action.payload;
+            if(state.items[product.id]){
+                return {
+                    ...state,
+                    items:{
+                        ...state.items,
+                        [product.id]:{
+                            ...state.items[product.id],
+                            quantity: state.items[product.id].quantity + 1
+                        }
+                    }
+                }
+            }
+            else{
+                return {
+                    ...state,
+                    items:{
+                        ...state.items,
+                        [product.id]:{
+                            ...product,
+                            quantity: 1
+                        }
+                    }
+                
+                }
+            }
         }
         case "REMOVE_FROM_CART":{
+            const product = action.payload;
+            if(state.items){
+                if(state.items[product.id].quantity > 1){
+                    return {
+                        ...state,
+                        items:{
+                            ...state.items,
+                            [product.id]:{
+                                ...state.items[product.id],
+                                quantity: state.items[product.id].quantity - 1
+                            }
+                        }
+                    }
+                }
+                else{
+                    // const newItems = {...state.items};
+                    // delete newItems[product.id];
+                    // return {
+                    //     ...state,
+                    //     items: newItems
+                    // }
+                    return {
+                        ...state,
+                        items: omit(state.items, product.id),
+                    }
+                }
+            }
+
 
         }
         default:
